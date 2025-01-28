93084dd1b9de73fc1ffcb6bafafdb13c73c45dd8
Nicmaa <nicoma.17fe@gmail.com>
2025-01-28 12:32:47 +0100

    Added DB connection and post request

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/Campgrounds');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error: "));
+db.once("open", () => {
+    console.log("Database connected!");
+})
+
 const app = express();
+
+const Camp = require('./models/campground');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.render('home.ejs');
+app.get('/campgrounds', async (req, res) => {
+    const camps = await Camp.find({});
+    res.render('allCamp.ejs', { camps });
 })
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('newCamp.ejs');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const newCamp = new Camp(req.body);
+    await newCamp.save();
+    res.redirect('/campgrounds');
+})
+
 app.listen(3000, () => {
     console.log("Port 3000 opened!");
 })
--- models/campground.js
+++ models/campground.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const campSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    location : {
+        type: String,
+        required: true,
+    },
+    description : {
+        type: String,
+    },
+    price : {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+})
+
+const Camp = mongoose.model('Camp', campSchema);
+
+module.exports = Camp;
